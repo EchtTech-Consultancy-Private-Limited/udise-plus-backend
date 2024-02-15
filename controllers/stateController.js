db48487e4e518642041df5ee4024e875b4e9cc48
chahal1997 <mesingh9719@gmail.com>
2024-02-15 16:42:45 +0530

    return unique states data

--- controllers/stateController.js
+++ controllers/stateController.js
@@ -1,17 +1,23 @@
-
+const { Sequelize } = require('sequelize');
 const State = require('../models/stateModel');
 
-
 async function getAllStates(req, res) {
-    try {
-      const states = await State.findAll();
-      res.json(states);
-    } catch (error) {
-      console.error('Error fetching states:', error.message);
-      res.status(500).json({ error: 'Internal server error' });
-    }
+  try {
+    const states = await State.findAll({
+      attributes: ['state_master.*'],
+      where: Sequelize.literal('id IN (SELECT MIN(id) FROM state_master GROUP BY state_name)'),
+      raw: true
+    });
+
+    res.json(states);
+  } catch (error) {
+    console.error('Error fetching states:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 }
 
+
+
 module.exports = {
-    getAllStates,
+  getAllStates,
 };

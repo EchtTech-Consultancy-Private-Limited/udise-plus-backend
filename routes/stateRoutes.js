ee6a61272cdd3f52cbd71a4e80d8cada24da53a9
suraj.echttech <suraj.chauhan@echttech.com>
2024-02-20 16:52:50 +0530

    pre-changed

--- routes/stateRoutes.js
+++ routes/stateRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const stateController = require('../controllers/stateController');
 
+router.get('/test', stateController.test);
 router.get('/get-all', stateController.getAllStates);
 
 

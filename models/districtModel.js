ab61ae5f84446ec34f806d3dba48e1a5741cc29f
suraj.echttech <suraj.chauhan@echttech.com>
2024-02-20 15:31:12 +0530

    created api for get district by state id

--- models/districtModel.js
+++ models/districtModel.js
@@ -4,9 +4,16 @@ const client = require('../db/dbconfig')
 class DistrictDataModel {
     tableName = 'district_master';
 
-    findAll = async (id) => {
+    findAll = async () => {
       
         const sql = `SELECT * FROM ${this.tableName}`;
+        const result = await client.query(sql);
+        return result.rows;
+    }
+
+    findById = async (state_id) => {
+      
+        const sql = `SELECT * FROM ${this.tableName} where state_id = ${state_id}`;
         const result = await client.query(sql);
         return result.rows;
     }
